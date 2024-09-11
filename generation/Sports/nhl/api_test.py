556c0324262aff99392b099211f088541196b81f
James <79486681+james-ngai@users.noreply.github.com>
2024-09-11 17:50:41 -0400

    Sports cleaned again

--- generation/Sports/nhl/api_test.py
+++ generation/Sports/nhl/api_test.py
@@ -7,46 +7,55 @@ class TestNHLApi(unittest.TestCase):
         response = get_player_spotlight()
         self.assertEqual(response.status_code, 200)
         self.assertIsInstance(response.json(), list)
+        self.assertNotIn('error', response)
 
     def test_get_standings(self):
         response = get_standings('2023-11-08')
         self.assertEqual(response.status_code, 200)
         self.assertIsInstance(response.json(), dict)
+        self.assertNotIn('error', response)
 
     def test_get_standings_season(self):
         response = get_standings_season()
         self.assertEqual(response.status_code, 200)
         self.assertIsInstance(response.json(), dict)
+        self.assertNotIn('error', response)
 
     def test_get_player_game_log(self):
         response = get_player_game_log(8476453, 20232024, 2)
         self.assertEqual(response.status_code, 200)
         self.assertIsInstance(response.json(), dict)
+        self.assertNotIn('error', response)
 
     def test_get_player_landing(self):
         response = get_player_landing(8476453)
         self.assertEqual(response.status_code, 200)
         self.assertIsInstance(response.json(), dict)
+        self.assertNotIn('error', response)
 
     def test_get_score(self):
         response = get_score('2023-11-08')
         self.assertEqual(response.status_code, 200)
         self.assertIsInstance(response.json(), dict)
+        self.assertNotIn('error', response)
 
     def test_get_location(self):
         response = get_location()
         self.assertEqual(response.status_code, 200)
         self.assertIsInstance(response.json(), dict)
+        self.assertNotIn('error', response)
 
     def test_get_schedule(self):
         response = get_schedule('2023-11-08')
         self.assertEqual(response.status_code, 200)
         self.assertIsInstance(response.json(), dict)
+        self.assertNotIn('error', response)
 
     def test_get_schedule_calendar(self):
         response = get_schedule_calendar('2023-11-08')
         self.assertEqual(response.status_code, 200)
         self.assertIsInstance(response.json(), dict)
+        self.assertNotIn('error', response)
 
 if __name__ == '__main__':
     unittest.main()
--- generation/Sports/soccerdata/api_test.py
+++ generation/Sports/soccerdata/api_test.py
@@ -1,11 +1,5 @@
 import unittest
 from api import list_competitions
-# from api import (
-#     get_area_by_id, list_areas, get_competition_by_code, list_competitions, 
-#     get_competition_standings, get_matches_for_competition, get_team_info,
-#     list_teams, get_player_info, list_matches_across_competitions,
-#     get_match_by_id, list_head2head_matches
-# )
 
 class TestSoccerDataAPI(unittest.TestCase):
     def test_list_competitions(self):
